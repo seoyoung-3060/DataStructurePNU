30f5ebc9ce1865aefda013857305fe13bfa13701
seoyoung-3060 <ksy654333@gmail.com>
2021-09-06 15:56:14 +0900

    385Byte

---
+++ Tiling.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> array;
+    int input;
+    while(cin >> input && input!=0) array.push_back(input);
+    
+    int size=array[0]+1;
+    for(int i=0; i<size; i++) {
+        int count = 0;
+        for (int j = 0; j < array.size(); j++)
+            if (array[j] >= (i+1)) count++;
+        cout << count << ' ';
+    }
+    return 0;
+}
